94868b8715ee28d4cf1ad670ae32caf930d84ff1
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-21 01:47:22 +0100

    Update UciUciCommand.java
    UCI `uci` command implementation

--- src/Magpie/UciUciCommand.java
+++ src/Magpie/UciUciCommand.java
@@ -1,3 +1,13 @@
+/*
+uci
+	tell engine to use the uci (universal chess interface),
+	this will be sent once as a first command after program boot
+	to tell the engine to switch to uci mode.
+	After receiving the uci command the engine must identify itself with the "id" command
+	and send the "option" commands to tell the GUI which engine settings the engine supports if any.
+	After that the engine should send "uciok" to acknowledge the uci mode.
+	If no uciok is sent within a certain time period, the engine task will be killed by the GUI.
+*/
 public class UciUciCommand extends UciCommand
 {
     public UciUciCommand(IBoard board)
@@ -5,5 +15,9 @@ public class UciUciCommand extends UciCommand
         super(board);
     }
     
-    
+    public void run() {
+        new UciIdResponse("Magpie", "Lucia Ocean Bartschick, Linus Nagel").send();
+        new UciOptionResponse().send();
+        new UciUciokResponse().send();
+    }    
 }
