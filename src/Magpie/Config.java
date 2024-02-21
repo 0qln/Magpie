38904be45d9fd6440ddb8a4046c00b0ed2620330
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-21 01:45:47 +0100

    Add Options

--- src/Magpie/Config.java
+++ src/Magpie/Config.java
@@ -1,4 +1,17 @@
+import java.util.*;
+
 public class Config
 {
     public static boolean Debug = false;
+    
+    public static final UciOption<?>[] Options = {
+    };
+ 
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<UciOption<T>> getOption(String name) {
+        return Arrays.stream(Options)
+            .filter(opt -> name.equals(opt.getName()))
+            .map(opt -> (UciOption<T>)opt)
+            .findFirst();
+    }
 }
