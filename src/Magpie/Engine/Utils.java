4ec7fbcc30238e91af6ebb08aefc89cab0abaec3
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-23 20:40:02 +0100

    Update Utils.java

--- src/Magpie/Engine/Utils.java
+++ src/Magpie/Engine/Utils.java
@@ -1,7 +1,5 @@
 package Engine;
 
-import java.util.function.Predicate;
-
 public final class Utils
 {
     public static int toSquareIndex(String square) {
@@ -12,5 +10,53 @@ public final class Utils
 
     public static int sqaureIndex0(int rank, int file) {
         return file + 8 * rank;
+    }
+
+    public static long deactivateBit(long board, int squareIdx) {
+        return board & ~(1L << squareIdx);
+    }
+
+    public static long activateBit(long board, int squareIdx) {
+        return board | (1L << squareIdx);
+    }
+
+    public static long flipBit(long board, int squareIdx) {
+        return board ^ (1L << squareIdx);
+    }
+
+    public static long flipBits(long board, int squareIdx1, int squareIdx2) {
+        return board ^ (1L << squareIdx1 | 1L << squareIdx2);
+    }
+
+    public static boolean getBit(long board, int squareIdx) {
+        return ((board >> squareIdx) & 1) == 1;
+    }
+
+    public static long setBit(long board, int squareIdx, boolean value) {
+        return (board & ~(1L << squareIdx)) | ((value ? 1L : 0L) << squareIdx);
+    }
+
+    public static void deactivateBit(long[] boards, int index, int squareIdx) {
+        boards[index] &= ~(1L << squareIdx);
+    }
+
+    public static void activateBit(long[] boards, int index, int squareIdx) {
+        boards[index] |= (1L << squareIdx);
+    }
+
+    public static void flipBit(long[] boards, int index, int squareIdx) {
+        boards[index] ^= (1L << squareIdx);
+    }
+
+    public static void flipBits(long[] boards, int index, int squareIdx1, int squareIdx2) {
+        boards[index] ^= (1L << squareIdx1 | 1L << squareIdx2);
+    }
+
+    public static boolean getBit(long[] boards, int index, int squareIdx) {
+        return ((boards[index] >> squareIdx) & 1) == 1;
+    }
+
+    public static void setBit(long[] boards, int index, int squareIdx, boolean value) {
+        boards[index] = (boards[index] & ~(1L << squareIdx)) | ((value ? 1L : 0L) << squareIdx);
     }
 }
