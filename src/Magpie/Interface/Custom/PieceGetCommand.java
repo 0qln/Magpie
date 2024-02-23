fb54c24b7f475a320c6ab711e34acc2d5fe3c9a5
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-23 23:45:21 +0100

    Create PieceGetCommand.java

---
+++ src/Magpie/Interface/Custom/PieceGetCommand.java
@@ -0,0 +1,14 @@
+package Interface.Custom;
+
+import Engine.IBoard;
+
+public class PieceGetCommand extends PieceCommand {
+
+    public PieceGetCommand(IBoard board, int square) {
+        super(board, square);
+    }
+    
+    public void run() {
+
+    }
+}
