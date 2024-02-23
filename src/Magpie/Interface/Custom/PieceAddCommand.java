a985c071a1b443f05bc338f9d07ac6bf8f1627af
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-23 23:45:24 +0100

    Create PieceAddCommand.java

---
+++ src/Magpie/Interface/Custom/PieceAddCommand.java
@@ -0,0 +1,15 @@
+package Interface.Custom;
+
+import Engine.IBoard;
+
+public class PieceAddCommand extends PieceCommand {
+    private int _piece;
+
+    public PieceAddCommand(IBoard board, int square, int piece) {
+        super(board, square);
+    }
+    
+    public void run() {
+        _board.addPiece(_square, _piece);
+    }
+}
