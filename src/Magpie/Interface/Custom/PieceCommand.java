25f887513708c66bfaa8c1af7b2da94d8509a184
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-23 23:45:11 +0100

    Create PieceCommand.java

---
+++ src/Magpie/Interface/Custom/PieceCommand.java
@@ -0,0 +1,18 @@
+package Interface.Custom;
+
+import java.util.Optional;
+
+import Engine.IBoard;
+
+/*
+ * piece [get <square> | add <square> <piece> | remove <square> ]
+ */
+public abstract class PieceCommand extends Command {
+
+    protected int _square;
+
+    public PieceCommand(IBoard board, int square) {
+        super(board);
+        _square = square;
+    }
+}
