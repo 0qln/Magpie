bbc47ff67615bcec2aebd383648a74828cebb7df
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-23 20:39:35 +0100

    Update Board.java

--- src/Magpie/Engine/Board.java
+++ src/Magpie/Engine/Board.java
@@ -4,9 +4,21 @@ import java.util.*;
 
 public class Board implements IBoard<Short>
 {
+    // Bitboard array to store the color of the pieces
     private long[] _cBitboards = new long[2];
-    private long[] _pBitboards = new long[7];
- 
+    // Bitboard array to store the types of the pieces
+    private long[] _tBitboards = new long[7];
+    // Piece array to store the pieces for each square
+    private int[] _pieces = new int[64];
+    // Counter for the pieces
+    private int[] _pieceCount = new int[14];
+    // The square where an en passant capture is possible, if non are possible -1
+    private int _epSquare = -1;
+    // The game ply
+    private int _ply = 0;
+    // The side the move
+    private int _turn = Color.White;
+    // A linked list to store the previous states of the game
     private LinkedList<BoardState> _stateStack = new LinkedList<BoardState>();
     
     
@@ -17,14 +29,13 @@ public class Board implements IBoard<Short>
 
     @Override
     public void makeMove(Short move) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+        BoardState bs = new BoardState();
+        _stateStack.push(bs);
     }
     
     @Override
     public void undoMove(Short move) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'undoMove'");
+        _stateStack.pop();
     }
 
 
@@ -50,9 +61,35 @@ public class Board implements IBoard<Short>
 
 
     @Override
-    public void setPiece(int square, int piece) {
-        // TODO Auto-generated method stub
+    public void addPiece(int square, int piece) {
+        assert(piece != Piece.None);
+
+        int type = Piece.getType(piece), color = Piece.getColor(piece);
+        int prevPiece = getPiece(square);
+
+        // tBitboards
+        Utils.deactivateBit(_tBitboards, Piece.getType(prevPiece), square);
+        Utils.activateBit(_tBitboards, type, square);
+
+        // cBitboards
+        Utils.deactivateBit(_cBitboards, Piece.getColor(prevPiece), square);
+        Utils.activateBit(_cBitboards, color, square);
+
+        // pieces
+        _pieces[square] = piece;
+
+        // piece count
+        _pieceCount[piece]++;
+    }
+
+    public void removePiece(int square) {
         
+    }
+
+
+    @Override
+    public int getPiece(int square) {
+        return _pieces[square];
     }
 
 
@@ -67,5 +104,22 @@ public class Board implements IBoard<Short>
     public void setTurn(int color) {
         // TODO Auto-generated method stub
         
+    }
+
+
+    public String toString() {
+        String result = "";
+        for (int rank = 7; rank >= 0; rank--) {
+            result += (rank + 1) + "  ";
+            for (int file = 0; file <= 7; file++) {
+                int square = Utils.sqaureIndex0(rank, file);
+                int piece = getPiece(square);
+                char c = Piece.toChar(piece);
+                result += c + " ";
+            }
+            result += "\n";
+        }
+        result += "   a b c d e f g h\n";
+        return result;
     }
 }
