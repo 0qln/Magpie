c809c1925ca4517cb8e61c776b3b90aa31b18a1c
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-24 21:48:25 +0100

    Update Title.java

--- src/Magpie/Interface/Title.java
+++ src/Magpie/Interface/Title.java
@@ -2,5 +2,14 @@ package Interface;
 
 public enum Title
 {
-    GM, IM, FM, WGM, WIM, none
+    GM, IM, FM, WGM, WIM, none;
+
+    public static Title parse(String input) {
+        for (Title title : Title.values()) {
+            if (title.name().equalsIgnoreCase(input)) {
+                return title;
+            }
+        }
+        return none;
+    }
 }
