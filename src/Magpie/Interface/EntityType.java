3779b1ddc30deb2b0df70d54bc6dd263d8e32861
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-24 21:48:28 +0100

    Update EntityType.java

--- src/Magpie/Interface/EntityType.java
+++ src/Magpie/Interface/EntityType.java
@@ -2,5 +2,14 @@ package Interface;
 
 public enum EntityType
 {
-    Computer, Human, Unknown
+    Computer, Human, Unknown;
+
+    public static EntityType parse(String input) {
+        for (EntityType type : EntityType.values()) {
+            if (type.name().equalsIgnoreCase(input)) {
+                return type;
+            }
+        }
+        return Unknown;
+    }
 }
