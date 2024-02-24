c903b7fac4800f6afa30183b4f5675c7d54db0be
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-24 20:34:39 +0100

    Create LoggerConfigurator.java

---
+++ src/Magpie/Misc/LoggerConfigurator.java
@@ -0,0 +1,27 @@
+package Misc;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class LoggerConfigurator {
+    public static Logger configureLogger(Class<?> clazz, String logFileName) {
+        Logger logger = Logger.getLogger(clazz.getName());
+        try {
+            Logger rootLogger = Logger.getLogger("");
+            // Remove the default console handler
+            Handler[] handlers = rootLogger.getHandlers();
+            for (Handler handler : handlers) {
+                if (handler instanceof ConsoleHandler) {
+                    rootLogger.removeHandler(handler);
+                }
+            }
+            // Add handler to the log file
+            FileHandler fileHandler = new FileHandler(logFileName, true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logger;
+    }
+}
