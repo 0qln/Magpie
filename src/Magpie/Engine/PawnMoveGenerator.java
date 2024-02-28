a32c9a34df7bedc6a40d66e8b57adbef8eefa71d
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-28 21:53:21 +0100

    Update PawnMoveGenerator.java
    En passant

--- src/Magpie/Engine/PawnMoveGenerator.java
+++ src/Magpie/Engine/PawnMoveGenerator.java
@@ -6,6 +6,8 @@ public class PawnMoveGenerator extends MoveGenerator
     private static final long RIGHT = 0xFEFEFEFEFEFEFEFEL;
     // Indexed by color
     private static final long[] STEP2 = new long[] { 0xFF00L, 0xFF000000000000L };
+    // Indexed by color
+    private static final long[] EP = new long[] { 0xFF0000L, 0xFF0000000000L };
 
     @Override
     int generate(short[] list, int index, Board board, int color) {
@@ -18,6 +20,7 @@ public class PawnMoveGenerator extends MoveGenerator
         final long pawns = board.getBitboard(PieceType.Pawn, Color.White);
         final long enemies = board.getCBitboard(Color.Black);
         final long pieces = enemies | board.getCBitboard(Color.White);
+        int from, to;
         long[] toBB = new long[1];
         long[] fromBB = new long[1];
         
@@ -27,8 +30,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] ^= toBB[0] & pieces; // Exclude occupied squares 
         fromBB[0] = toBB[0] >>> 8;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.QUIET_MOVE_FLAG);
         }
         
@@ -38,8 +41,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] ^= ((toBB[0] >>> 8) & pieces) << 8;
         fromBB[0] = toBB[0] >>> 16;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.DOUBLE_PAWN_PUSH_FLAG);
         }
 
@@ -47,8 +50,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] = ((pawns & RIGHT) << 7) & enemies;
         fromBB[0] = toBB[0] >>> 7;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
         }
  
@@ -56,11 +59,22 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] = ((pawns & LEFT) << 9) & enemies;
         fromBB[0] = toBB[0] >>> 9;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
         }
         
+        // En passant 
+        // Check left and right of the en passant square for an ally pawn
+        toBB[0] = 1L << board.getEnPassantSquare();
+        to = Utils.lsb(toBB[0]);
+        fromBB[0] = (((pawns & LEFT) << 9) & toBB[0]) >>> 9; 
+        fromBB[0] |= (((pawns & RIGHT) << 7) & toBB[0]) >>> 7;
+        while (fromBB[0] != 0) {
+            from = Utils.popLsb(fromBB);
+            list[index++] = Move.create(from, to, Move.EN_PASSANT_FLAG);
+        }
+
         return index;
     }
 
@@ -68,6 +82,7 @@ public class PawnMoveGenerator extends MoveGenerator
         final long pawns = board.getBitboard(PieceType.Pawn, Color.Black);
         final long enemies = board.getCBitboard(Color.White);
         final long pieces = enemies | board.getCBitboard(Color.Black);
+        int from, to;
         long[] toBB = new long[1];
         long[] fromBB = new long[1];
         
@@ -77,8 +92,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] ^= toBB[0] & pieces; // Exclude occupied squares 
         fromBB[0] = toBB[0] << 8;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.QUIET_MOVE_FLAG);
         }
         
@@ -88,8 +103,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] ^= ((toBB[0] << 8) & pieces) >>> 8;
         fromBB[0] = toBB[0] << 16;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.DOUBLE_PAWN_PUSH_FLAG);
         }
 
@@ -97,8 +112,8 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] = ((pawns & RIGHT) >>> 9) & enemies;
         fromBB[0] = toBB[0] << 9;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
         }
  
@@ -106,11 +121,22 @@ public class PawnMoveGenerator extends MoveGenerator
         toBB[0] = ((pawns & LEFT) >>> 7) & enemies;
         fromBB[0] = toBB[0] << 7;
         while (toBB[0] != 0) {
-            final int from = Utils.popLsb(fromBB);
-            final int to = Utils.popLsb(toBB);
+            from = Utils.popLsb(fromBB);
+            to = Utils.popLsb(toBB);
             list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
         }
-        
+
+        // En passant 
+        // Check left and right of the en passant square for an ally pawn
+        toBB[0] = 1L << board.getEnPassantSquare();
+        to = Utils.lsb(toBB[0]);
+        fromBB[0] = (((pawns & LEFT) >>> 7) & toBB[0]) << 7; 
+        fromBB[0] |= (((pawns & RIGHT) >>> 9) & toBB[0]) << 9;
+        while (fromBB[0] != 0) {
+            from = Utils.popLsb(fromBB);
+            list[index++] = Move.create(from, to, Move.EN_PASSANT_FLAG);
+        }
+       
         return index;
     }
 }
