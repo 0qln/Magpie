008a6855ba5eb206eff0afda83bd99c405ae2a7e
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-28 20:06:09 +0100

    Continue pawn move gen implementation

--- src/Magpie/Engine/PawnMoveGenerator.java
+++ src/Magpie/Engine/PawnMoveGenerator.java
@@ -15,32 +15,50 @@ public class PawnMoveGenerator extends MoveGenerator
     }
 
     private int white(short[] list, int index, Board board) {
-        Utils.printBB(LEFT);
-        Utils.printBB(RIGHT);
-        long[] pawnsBB = new long[] { board.getBitboard(PieceType.Pawn, Color.White) };
+        final long PAWNS = board.getBitboard(PieceType.Pawn, Color.White);
         final long enemies = board.getCBitboard(Color.Black);
-        Utils.printBB(pawnsBB[0]);
-        long[] rightBB = new long[] { ((pawnsBB[0] & RIGHT) << 7) & enemies };
-        long[] leftBB = new long[] { ((pawnsBB[0] & LEFT) << 9) & enemies };
-        long[] singleBB = new long[] { pawnsBB[0] << 8 };
-        long[] doubleBB = new long[] { (pawnsBB[0] & DOUBLE[Color.White]) << 16 };
-        Utils.printBB(singleBB[0] | leftBB[0] | rightBB[0] | doubleBB[0]);
+        final long pieces = enemies | board.getCBitboard(Color.White);
+        long[] ptr = new long[1];
+        long[] bb = new long[1];
+        
+        // Single step
+        bb[0] = PAWNS;
+        Utils.printBB(pieces);
+        ptr[0] = (PAWNS << 8);
+        ptr[0] ^= ptr[0] & pieces; // Exclude occupied squares 
+        while (ptr[0] != 0) {
+            final int from = Utils.popLsb(bb);
+            final int to = Utils.popLsb(ptr);
+            list[index++] = Move.create(from, to, Move.QUIET_MOVE_FLAG);
+        }
 
-        while (true) {
-            final int from = Utils.popLsb(pawnsBB);
-            final int single = Utils.popLsb(singleBB);
-            final int double_ = Utils.popLsb(doubleBB);
-            final int left = Utils.popLsb(leftBB);
-            final int right = Utils.popLsb(rightBB);
+        // Double step
+        bb[0] = PAWNS;
+        ptr[0] = ((PAWNS & DOUBLE[Color.White]) << 16);
+        ptr[0] ^= ptr[0] & pieces; 
+        ptr[0] ^= ((ptr[0] >> 8) & pieces) << 8;
+        while (ptr[0] != 0) {
+            final int from = Utils.popLsb(bb);
+            final int to = Utils.popLsb(ptr);
+            list[index++] = Move.create(from, to, Move.DOUBLE_PAWN_PUSH_FLAG);
+        }
 
-            list[index++] = Move.create(from, single, Move.QUIET_MOVE_FLAG);
-            list[index++] = Move.create(from, left, Move.QUIET_MOVE_FLAG);
-            list[index++] = Move.create(from, right, Move.QUIET_MOVE_FLAG);
-            list[index++] = Move.create(from, double_, Move.DOUBLE_PAWN_PUSH_FLAG);
-
-            if (pawnsBB[0] == 0) {
-                break;
-            }
+        // Capture right
+        bb[0] = PAWNS;
+        ptr[0] = ((PAWNS & RIGHT) << 7) & enemies;
+        while (ptr[0] != 0) {
+            final int from = Utils.popLsb(bb);
+            final int to = Utils.popLsb(ptr);
+            list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
+        }
+ 
+        // Capture left
+        bb[0] = PAWNS;
+        ptr[0] = ((PAWNS & LEFT) << 9) & enemies;
+        while (ptr[0] != 0) {
+            final int from = Utils.popLsb(bb);
+            final int to = Utils.popLsb(ptr);
+            list[index++] = Move.create(from, to, Move.CAPTURE_FLAG);
         }
         
         return index;
