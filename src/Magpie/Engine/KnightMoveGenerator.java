3f4ea36d2c2c6b8ffb77c889285e1b26c1280ba6
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-28 21:53:25 +0100

    Create KnightMoveGenerator.java

---
+++ src/Magpie/Engine/KnightMoveGenerator.java
@@ -0,0 +1,11 @@
+package Engine;
+
+public class KnightMoveGenerator extends MoveGenerator {
+
+    @Override
+    int generate(short[] list, int index, Board board, int color) {
+        // TODO Auto-generated method stub
+        return index;
+    }
+
+}
