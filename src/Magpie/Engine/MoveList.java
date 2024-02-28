00ce9ba0f9a8b8053f01f7def12ad7d51384a13f
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-28 17:55:34 +0100

    Create MoveList.java

---
+++ src/Magpie/Engine/MoveList.java
@@ -0,0 +1,23 @@
+package Engine;
+
+import java.util.Arrays;
+
+public class MoveList {
+    private final short[] _moves = new short[256];
+    private int _moveCount = 0;
+
+
+    public static MoveList generate(Board board) {
+        MoveList list = new MoveList();
+
+        // Generate using generator classes
+        list._moveCount = new PawnMoveGenerator().generate(list._moves, list._moveCount, board, board.getTurn());
+
+        return list;
+    }
+
+    public short[] getMoves() {
+        // TODO? Remove overhead caused by copying
+        return Arrays.copyOfRange(_moves, 0, _moveCount);
+    }
+}
