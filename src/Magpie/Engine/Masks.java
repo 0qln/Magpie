40d7256fd9c9aff144cf4c63d5626dc6bcabe05d
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-02-29 20:08:08 +0100

    Update Masks.java

--- src/Magpie/Engine/Masks.java
+++ src/Magpie/Engine/Masks.java
@@ -2,6 +2,28 @@ package Engine;
 
 public final class Masks {
 
+    public static final long[] Files = {
+        0x0101010101010101L,
+        0x0202020202020202L,
+        0x0404040404040404L,
+        0x0808080808080808L,
+        0x1010101010101010L,
+        0x2020202020202020L,
+        0x4040404040404040L,
+        0x8080808080808080L,
+    };
+
+    public static final long[] Ranks = {
+        0x00000000000000FFL,
+        0x000000000000FF00L,
+        0x0000000000FF0000L,
+        0x00000000FF000000L,
+        0x000000FF00000000L,
+        0x0000FF0000000000L,
+        0x00FF000000000000L,
+        0xFF00000000000000L,
+    };
+
     public static final long 
         East = 0x7F7F7F7F7F7F7F7FL,
         EaEa = 0x3F3F3F3F3F3F3F3FL,
