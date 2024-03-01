74285e408de269f821fff5ce36caf65876a14307
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-03-02 00:41:05 +0100

    Double check evasion

--- src/Magpie/Engine/MoveList.java
+++ src/Magpie/Engine/MoveList.java
@@ -1,5 +1,7 @@
 package Engine;
 
+import static Engine.Utils.popLsb;
+import static Engine.Utils.printBB;
 import static Engine.Utils.target;
 
 import java.util.Arrays;
@@ -34,6 +36,8 @@ public class MoveList {
     public static MoveList checkResolves(Board board) {
         MoveList list = new MoveList();
 
+        // list._moveCount = new Move
+
         return list;
     }
 
@@ -56,26 +60,29 @@ public class MoveList {
         // 1. Generate pseudo legal moves.
 
         // temporary
-        // candidates = pseudoLegal(board);
+        candidates = pseudoLegal(board);
 
-        // 1.1 If in check, generate only pseudo legal moves that resolve the check
-        if (board.isInSingleCheck()) {
-            candidates = checkResolves(board);
-        }
-        // 1.2 If in double check, generate only pseudo legal king moves
-        else if (board.isInDoubleCheck()) {
-            candidates = checkResolvesByKing(board);
-        }
-        // 1.3 Generate all pseudo legal quiet moves and captures
-        else {
-            candidates = pseudoLegal(board);
-        }
+        // // 1.1 If in check, generate only pseudo legal moves that resolve the check
+        // if (board.isInSingleCheck()) {
+        //     candidates = checkResolves(board);
+        //     System.out.println("Single check");
+        // }
+        // // 1.2 If in double check, generate only pseudo legal king moves
+        // else if (board.isInDoubleCheck()) {
+        //     candidates = checkResolvesByKing(board);
+        //     System.out.println("Double check");
+        // }
+        // // 1.3 Generate all pseudo legal quiet moves and captures
+        // else {
+        //     candidates = pseudoLegal(board);
+        //     System.out.println("No check");
+        // }
 
         // 2. Filter out pseudo legal moves that are not legal.
         for (int i = 0; i < candidates._moveCount; i++) {
             final short move = candidates._moves[i];
             final int from = Move.getFrom(move), to = Move.getTo(move), flag = Move.getFlag(move);
-            final int movingP = board.getPiece(to);
+            final int movingP = board.getPiece(from);
             final long kingBB = board.getBitboard(PieceType.King, board.getTurn());
             final long nstm = board.getNstmAttacks();
 
@@ -92,15 +99,41 @@ public class MoveList {
             }
 
             // 2.1 King moving into a check is not legal
-            if (Piece.getType(movingP) == PieceType.King
-                    && (target(to) & nstm) != 0) {
-                // continue;
+            if (Piece.getType(movingP) == PieceType.King) {
+                final long updatedNstm = nstm | slidingAttacks(
+                        board, new long[] { board.getCheckers() }, board.getOccupancy() ^ kingBB);
+                printBB(updatedNstm);
+                if ((target(to) & updatedNstm) != 0) {
+                    continue;
+                }
             }
 
             list._moves[list._moveCount++] = candidates._moves[i];
         }
 
         return list;
+    }
+
+    private static final long slidingAttacks(Board board, long[] sliders, long occupied) {
+        long result = 0;
+        while (sliders[0] != 0)
+            result |= slidingAttack(board, popLsb(sliders), occupied);
+        return result;
+    }
+
+    private static final long slidingAttack(Board board, int square, long occupied) {
+        int type = Piece.getType(board.getPiece(square));
+        switch (type) {
+            case PieceType.Bishop:
+                return BishopMoveGenerator.attacks(square, occupied);
+            case PieceType.Rook:
+                return RookMoveGenerator.attacks(square, occupied);
+            case PieceType.Queen:
+                return QueenMoveGenerator.attacks(square, occupied);
+
+            default:
+                return 0;
+        }
     }
 
     public short[] getMoves() {
