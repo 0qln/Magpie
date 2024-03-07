9c1da8a96ed1f6b4a2aaeaf9d7d220173c358211
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-03-07 01:15:20 +0100

    Update Main.java

--- src/Magpie/Interface/Main.java
+++ src/Magpie/Interface/Main.java
@@ -6,8 +6,7 @@ import java.util.logging.Logger;
 import Misc.LoggerConfigurator;
 import Misc.Ptr;
 
-public final class Main
-{
+public final class Main {
     static {
         // Fuck java
         new UciCommand();
@@ -24,17 +23,24 @@ public final class Main
     public static Scanner scanner = new Scanner(System.in);
     private static Logger logger = LoggerConfigurator.configureLogger(Main.class);
 
-    public static void main(String[] args) throws Misc.Builder.FieldNotSetException
-    {
+    public static void main(String[] args) throws Misc.Builder.FieldNotSetException {
         scanner.useDelimiter("\n");
         Ptr<Engine.IBoard> board = Ptr.to(new Engine.Board.Builder().build());
-        CommandParser parser = new CommandParser();
+
+        // Set up startposition
+        handleInput("position startpos", board);
+
         while (true) {
             String input = scanner.next();
             logger.info("Input: " + input);
-            Optional<ICommandBuilder> commandBuilder = parser.parse(input);
-            commandBuilder.ifPresent(
-                // UCI:: The engine must always be able to process input from stdin, even while thinking.
+            handleInput(input, board);
+        }
+    }
+
+    private static final void handleInput(String input, Ptr<Engine.IBoard> board) {
+        CommandParser.parse(input).ifPresent(
+                // UCI:: The engine must always be able to process input from stdin, even while
+                // thinking.
                 builder -> {
                     logger.info("Command builder: " + builder.getClass().getName());
                     ICommand command = builder.buildForBoard(board);
@@ -42,8 +48,6 @@ public final class Main
                     if (command.canRun()) {
                         command.run();
                     }
-                }
-            );
-        }
+                });
     }
 }
