49214988ea6af5a411cffe7acd3c35f3cc4affbc
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-03-08 01:14:56 +0100

    Update StaticEvaluator.java

--- src/Magpie/Engine/StaticEvaluator.java
+++ src/Magpie/Engine/StaticEvaluator.java
@@ -10,6 +10,11 @@ public final class StaticEvaluator {
             0, 100, 300, 350, 500, 900, 10_000
     };
 
+    public static final int PHASE_TOTAL = 24;
+    public static final int[] PHASE_VALUES = {
+            0, 0, 1, 1, 2, 4, 0
+    };
+
     private final Board _board;
 
     public StaticEvaluator(Board board) {
@@ -31,6 +36,20 @@ public final class StaticEvaluator {
         // result += 15;
 
         return result;
+    }
+
+    public final static int phase(Board board) {
+        int result = 0;
+        result += PHASE_VALUES[PieceType.Knight] * countBits(board.getTBitboard(PieceType.Knight));
+        result += PHASE_VALUES[PieceType.Bishop] * countBits(board.getTBitboard(PieceType.Bishop));
+        result += PHASE_VALUES[PieceType.Rook] * countBits(board.getTBitboard(PieceType.Rook));
+        result += PHASE_VALUES[PieceType.Queen] * countBits(board.getTBitboard(PieceType.Queen));
+        return result;
+    }
+
+    // TODO: check if this is right
+    public final static int taper(int phase, int mg, int eg) {
+        return Misc.Utils.lerp(mg, eg, phase, PHASE_TOTAL);
     }
 
 
