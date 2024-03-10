8757fb05623c122547b994e2cb9ca74d9b693adb
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-03-11 00:29:47 +0100

    Create TranspositionTable.java

---
+++ src/Magpie/Engine/TranspositionTable.java
@@ -0,0 +1,63 @@
+package Engine;
+
+import Engine.AlphaBetaSearchTree.Line;
+
+public class TranspositionTable {
+    private final Entry[] _entries;
+    private final int _size;
+
+    public TranspositionTable(int size) {
+        _size = size;
+        _entries = new Entry[size];
+    }
+
+    public void set(Entry entry) {
+        int index = (int) Math.abs(entry.key % _size);
+        Entry oldEntry = _entries[index];
+
+        if (oldEntry != null) {
+            if (entry.type < oldEntry.type || entry.depth >= oldEntry.depth) {
+                _entries[index] = entry;
+            }
+        }
+        else {
+            _entries[index] = entry;
+        }
+    }
+
+    public Entry get(long key, DepthLevelInfo info) {
+        Entry entry = _entries[(int) Math.abs(key % _size)];
+        if (entry.key != key) entry = null;
+        info.tt_entry = entry == null;
+        return entry;
+    }
+
+    public static class Entry {
+        // ~16 Bytes
+        public static final byte SIZE_BYTES = 16;
+
+        /**
+         * 0 - PV node (exact)
+         * 1 - All node (Upper bound)
+         * 2 - Cut node (Lower bound, beta cutoff)
+         */
+        public static final int TYPE_PV = 0;
+        public static final int TYPE_ALL = 1;
+        public static final int TYPE_CUT = 2;
+
+        public long key;
+        public byte depth;
+        public int score; 
+        public byte type;
+        public Line pv;
+
+        public Entry(long key, byte depth, int score, byte type, Line pv) {
+            this.key = key;
+            this.depth = depth;
+            this.score = score;
+            this.type = type;
+            this.pv = pv;
+        }
+    }
+
+}
