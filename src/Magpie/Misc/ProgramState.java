0740c1ad8e02c63c6fb2bacfc2f922d0a87fbc06
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-23 01:54:42 +0200

    Update ProgramState.java

--- src/Magpie/Misc/ProgramState.java
+++ src/Magpie/Misc/ProgramState.java
@@ -1,7 +1,10 @@
 package Misc;
 
+import java.util.ArrayList;
+
 public class ProgramState
 {
     public final Ptr<Engine.IBoard> board = new Ptr<>(null);
     public final Ptr<Engine.ISearchTree> search = new Ptr<>(null);
+    public final ArrayList<Interface.TestCommand> runningTests = new ArrayList<>();
 }
