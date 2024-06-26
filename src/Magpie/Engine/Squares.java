d94ff153420cd5ff6ed99b9bdce7dbbbb68a65a2
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-26 18:42:09 +0200

    Create Squares.java

---
+++ src/Magpie/Engine/Squares.java
@@ -0,0 +1,13 @@
+package Engine;
+
+import static Misc.Utils.toSquareIndex;
+
+public class Squares {
+    
+    public static final int 
+        A1 = toSquareIndex("a1"),
+        H1 = toSquareIndex("h1"),
+        A8 = toSquareIndex("a8"),
+        H8 = toSquareIndex("h8");
+
+}
