285c54f9548c86106e2e6eecd72c2a3de053549d
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-06-26 18:42:26 +0200

    Update Color.java

--- src/Magpie/Engine/Color.java
+++ src/Magpie/Engine/Color.java
@@ -6,6 +6,6 @@ public final class Color {
     public static final int Black = 1;
 
     public static int NOT(int color) {
-        return color == White ? Black : White;
+        return color ^ 1;
     }
 }
